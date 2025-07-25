8d1a25ff5a145f0607b4b9dc884cf0a4c6906ea1
Mohammad Harman Rashidi <harman.rashidi@walkingtree.tech>
2025-07-25 11:17:21 +0530

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Admin } from './components/admin/admin';
+import { ControFlow } from './components/contro-flow/contro-flow';
+import { DataBinding } from './components/data-binding/data-binding';
+import { SignalEx } from './components/signal-ex/signal-ex';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'admin',
+        component: Admin
+    },
+    {
+        path: 'control-flow',
+        component: ControFlow
+    },
+    {
+        path: 'data-binding',
+        component: DataBinding
+    },
+    {
+        path: 'signal',
+        component: SignalEx
+    }
+];
