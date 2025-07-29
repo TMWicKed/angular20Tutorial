262bba964a2af21dff4549e0a26dfb220a0307fa
Mohammad Harman Rashidi <harman.rashidi@walkingtree.tech>
2025-07-29 13:12:21 +0530

    directives

--- src/app/components/att-directive/att-directive.ts
+++ src/app/components/att-directive/att-directive.ts
@@ -1,18 +1,25 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css'
 })
 export class AttDirective {
 
-  div1ClassName = signal<string>("");
+  div1ClassName = signal<string>("" );
+
+  isDiv2Green = signal<boolean>(false);
 
   setBgClass(str: string) {
     this.div1ClassName.set(str);
+  }
+
+
+  toggleDiv2Color() {
+    this.isDiv2Green.set(!this.isDiv2Green());
   }
 
 }
