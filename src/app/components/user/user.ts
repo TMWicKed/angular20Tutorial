8f998bdbf7ed3c49fc30abc43a669411ccd0c1e7
Mohammad Harman Rashidi <harman.rashidi@walkingtree.tech>
2025-08-14 12:18:20 +0530

    Post api template forms in angular

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
+export class User implements OnInit{
 
+  userList: any[] = [];
+
+  userObject: any = {
+  "userId": 0,
+  "emailId": "string",
+  "password": "string",
+  "fullName": "string",
+  "mobileNo": "string"
+  };
+
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers").subscribe((res:any)=>{
+      this.userList = res;
+    }, (error:any)=>{
+      console.error("Error fetching users:", error);
+    });
+  }
+
+  onSaveUser() {
+    debugger
+    this.http.post("https://api.freeprojectapi.com/api/GoalTracker/register", this.userObject).subscribe({
+      next:(result) => {
+        debugger
+        alert("User saved successfully");
+        this.getUsers();
+      },
+      error:(error) => {
+        debugger
+        alert("Error saving user: " + error.message);
+      }
+    })
+  }
 }
