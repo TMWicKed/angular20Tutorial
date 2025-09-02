ea21f261d50d907f0f015b8d089479cc1e692862
Mohammad Harman Rashidi <harman.rashidi@walkingtree.tech>
2025-09-02 13:19:45 +0530

    Default pipes

---
+++ src/app/components/pipe-ex/pipe-ex.ts
@@ -0,0 +1,34 @@
+import {
+  DatePipe,
+  JsonPipe,
+  LowerCasePipe,
+  SlicePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe-ex',
+  imports: [UpperCasePipe, LowerCasePipe, TitleCasePipe, DatePipe, SlicePipe,JsonPipe],
+  templateUrl: './pipe-ex.html',
+  styleUrl: './pipe-ex.css',
+})
+export class PipeEx {
+  courseName = 'angular 20';
+  courseDuration = '40 HOURS';
+  courseTitle = 'anGUlar 20 couRSe';
+  currentDate = new Date();
+  rollNoList: number[] = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110];
+  studentObj: any = {
+    name: 'John Doe',
+    age: 25,
+    course: 'Angular 20',
+    address: {
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'CA',
+      zip: '12345'
+    }
+  }
+}
