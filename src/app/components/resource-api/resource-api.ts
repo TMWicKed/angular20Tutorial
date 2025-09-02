1acf742d566e0024917f7c98008330bf05e2fd94
Mohammad Harman Rashidi <harman.rashidi@walkingtree.tech>
2025-09-02 15:09:59 +0530

    rxResource and reload used for resource which was not in HTTPClient

--- src/app/components/resource-api/resource-api.ts
+++ src/app/components/resource-api/resource-api.ts
@@ -1,4 +1,6 @@
-import { Component, resource } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, resource } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-resource-api',
@@ -8,10 +10,22 @@ import { Component, resource } from '@angular/core';
 })
 export class ResourceApi {
 
+  http = inject(HttpClient)
+
   userData = resource({
     loader: () => {
       return fetch("https://jsonplaceholder.typicode.com/users")
         .then(response => response.json());
     }
   })
+
+  userList = rxResource({
+    stream: () => this.http.get<any[]>("https://jsonplaceholder.typicode.com/users")
+  })
+
+  constructor() {
+    setTimeout(() => {
+      this.userData.reload();
+    }, 6000);
+  }
 }
